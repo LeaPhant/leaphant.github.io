0356684edf0e952f7f7e55182e268d262d2f6689
LeaPhant <mail@lea.moe>
2021-02-10 00:31:49 +0100

    Disable on unranked maps for now

--- userscripts/ranked-score.user.js
+++ userscripts/ranked-score.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         osu! ranked score
 // @namespace    https://leaphant.github.io/
-// @version      0.1
+// @version      0.2
 // @description  adds score for nomod ss to std beatmap pages
 // @author       LeaPhant
 // @match        http*://osu.ppy.sh/*
@@ -18,19 +18,23 @@ async function pageChange() {
 
     const mapId = hash.split('/')[1];
 
-    const beatmapInfo = await (await fetch(`https://osu.lea.moe/b/${mapId}`)).json();
-
     const rankedStateElement =
     document.querySelector('.beatmapset-header__box.beatmapset-header__box--stats > .beatmapset-status.beatmapset-status--show');
 
-    let rankedState = 'Ranked';
+    let rankedState = 'ranked';
 
     for (const childNode of rankedStateElement.childNodes) {
         if (childNode.nodeType === Node.TEXT_NODE) {
-            rankedState = childNode.textContent;
+            rankedState = childNode.textContent.toLowerCase().trim();
             break;
         }
     }
+    
+    if (!['ranked', 'qualified', 'approved', 'loved'].includes(rankedState)) {
+        return;
+    }
+    
+    const beatmapInfo = await (await fetch(`https://osu.lea.moe/b/${mapId}`)).json();
 
     rankedStateElement.innerHTML = `<span style="opacity:.85">${beatmapInfo.beatmap.max_score.toLocaleString()} </span>${rankedState}<span> Score</span>`;
 }
